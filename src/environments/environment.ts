26efe666f934983a52c05c9641f06725182e85d2
mpho-mulaudzi <christianmpho@gmail.com>
2022-05-16 11:04:44 +0200

    firebase:init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBz4O74mjEvrXu3t_vZ5X2_3r9O7-uiNis",
+    authDomain: "marvel-movies-bb871.firebaseapp.com",
+    projectId: "marvel-movies-bb871",
+    storageBucket: "marvel-movies-bb871.appspot.com",
+    messagingSenderId: "439818303910",
+    appId: "1:439818303910:web:22983afb1a48bcd42c3767"
+  }
+  
 };
