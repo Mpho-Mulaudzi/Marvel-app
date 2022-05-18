2067f2fca16c76e2dc1ca5ce145126018384f62b
mpho-mulaudzi <christianmpho@gmail.com>
2022-05-18 16:00:24 +0200

    fix:Ui responsive, add:Card Component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardComponent } from './card/card.component';
 
 
 
@@ -19,6 +20,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     FooterComponent,
     NavbarComponent,
     SidebarComponent,
+    CardComponent,
    
    
   ],
