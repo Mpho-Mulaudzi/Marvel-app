50d6a97f57aae320f2451fa339c84459ffb32108
mpho-mulaudzi <christianmpho@gmail.com>
2022-05-19 15:19:10 +0200

    fix:card display

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
+import { IMovies } from '../models/movie';
 import { MovieService } from '../services/movie.service';
 
 @Component({
@@ -7,12 +8,11 @@ import { MovieService } from '../services/movie.service';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  public movies=[];
 
+  @Input() movies: IMovies;
   constructor(private _movieservice:MovieService) { }
 
   ngOnInit(): void {
 
   }
-
 }
